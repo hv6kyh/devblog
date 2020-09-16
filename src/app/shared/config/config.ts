2d868011db2bef971c4bffd19a13ce4f8a6a088e
김영호 <hv6kyh@mediagram.co.kr>
2020-09-16 22:12:27 +0900

    운영환경용 api url 추가

--- src/app/shared/config/config.ts
+++ src/app/shared/config/config.ts
@@ -3,6 +3,7 @@ import { environment } from './../../../environments/environment';
 export let API_URL = null;
 
 if (environment.production) {
+  API_URL = 'https://api.kimyoungho.ml/api';
 } else {
   API_URL = 'http://localhost:3000/dev/api';
 }
