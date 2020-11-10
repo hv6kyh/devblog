5703bc9f541d39881103cac7d7c455824cbaa22c
김영호 <youngho@gim-yeonghoui-MacBookPro.local>
2020-11-10 16:50:45 +0900

    이미지 클릭으로 리사이즈

--- src/app/menu/post/detail/detail.component.ts
+++ src/app/menu/post/detail/detail.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit, OnDestroy, Injector, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, OnDestroy, Injector, AfterViewChecked } from '@angular/core';
 import { PostService } from '../post.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { APISuccess } from '../../../shared/constant/DTO/success.response';
 import { Post } from '../types';
+import { MarkdownService } from 'ngx-markdown';
 
 @Component({
   selector: 'ngx-detail',
   templateUrl: './detail.component.html',
   styleUrls: ['./detail.component.scss'],
 })
-export class DetailComponent implements OnInit, OnDestroy {
+export class DetailComponent implements OnInit, OnDestroy, AfterViewChecked {
   private postId: string;
   post: Post = null;
 
@@ -18,10 +19,13 @@ export class DetailComponent implements OnInit, OnDestroy {
   public router: Router = null;
   loadFinish = false;
 
+  imageEventFlag: boolean;
+
   constructor(
     private readonly postService: PostService,
     private readonly route: ActivatedRoute,
     private readonly injector: Injector,
+    private readonly markdownService: MarkdownService,
   ) {
     this.router = this.injector.get(Router);
   }
@@ -37,6 +41,38 @@ export class DetailComponent implements OnInit, OnDestroy {
         console.log('디테일 this.post: ', this.post);
       }
     });
+
+    // image 렌더러를 수정해서 width 속성 부여함
+    // 화면에 꽉차게
+    this.markdownService.renderer.image = (href: string, title: string, text: string) =>
+      `
+        <img class="image" src="${href}" alt="${text}" width="100%">
+      `;
+
+    this.imageEventFlag = false;
+  }
+
+  ngAfterViewChecked(): void {
+    if (!this.imageEventFlag) {
+      // 클래스로 element 찾음
+      const images = document.getElementsByClassName('image');
+
+      if (images.length) {
+        Array.from(images).forEach((el: HTMLImageElement) => {
+          // 순회하며 클릭 이벤트 부여
+          if (!el.onclick) {
+            el.onclick = () => {
+              if (el.width === el.naturalWidth) {
+                el.setAttribute('width', '100%');
+              } else {
+                el.width = el.naturalWidth;
+              }
+            };
+          }
+        });
+        this.imageEventFlag = true;
+      }
+    }
   }
 
   ngOnDestroy() {
